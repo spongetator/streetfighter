91fde18c228de069f3ad7ccaf15493f773a7a736
Beyonce Knowles <beyonce@thinkful.com>
2015-06-26 11:47:15 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    $('.hadouken').finish().show()
+  .animate(
+    {'left': '1020px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '520px');
+    }
+  );
+    // play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show().animate(
+     {'left': '1020px'},
+     500,
+  function() {
+    $(this).hide();
+    $(this).css('left', '520px');
+  }
+);
+    // show hadouken and animate it to the right of the screen
+  })
+  .mouseup(function() {
+  $('.ryu-throwing').hide();
+  $('.ryu-ready').show();
+    // ryu goes back to his ready position
+  });
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
