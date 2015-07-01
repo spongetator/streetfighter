8a133d831fa4f4513983be1bf69ce5723f404299
Beyonce Knowles <beyonce@thinkful.com>
2015-07-01 11:54:58 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -1,43 +1,45 @@
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
-    $('.ryu-still').show();
-  })
-  .mousedown(function() {
-    $('.hadouken').finish().show()
-  .animate(
-    {'left': '1020px'},
-    500,
-    function() {
-      $(this).hide();
-      $(this).css('left', '520px');
-    }
-  );
-    // play hadouken sound
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.hadouken').show().animate(
-     {'left': '1020px'},
-     500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-);
-    // show hadouken and animate it to the right of the screen
-  })
-  .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
-    // ryu goes back to his ready position
-  });
+
+    $(".ryu").mouseenter(function(){
+        $(".ryu-still").hide();
+        $(".ryu-ready").show();
+    })
+    .mouseleave(function(){
+        $(".ryu-ready").hide();
+        $(".ryu-still").show();
+    })
+    .mousedown(function() {
+            playHadouken();
+            $(".ryu-ready").hide();
+            $(".ryu-throwing").show();
+            $(".hadouken").finish().show()
+                .animate(
+                    {"left": "300px"},
+                    500,
+                    function() {
+                        $(this).hide();
+                        $(this).css("left", "-160px");
+                    }   
+                );
+    })
+    .mouseup(function() {
+            $(".ryu-throwing").hide();
+            $(".ryu-ready").show();
+    });
+    $("body").keydown(function(event){
+        if(event.keyCode == 88) {
+            $(".ryu").hide();
+            $(".ryu-cool").show();
+        }
+    });
+    $("body").keyup(function(){
+        $(".ryu-cool").hide();
+        $(".ryu").show();
+    });
 });
+
 function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+    $("#hadouken-sound") [0].volume = 0.5;
+    $("#hadouken-sound") [0].load();
+    $("#hadouken-sound") [0].play();
 }
